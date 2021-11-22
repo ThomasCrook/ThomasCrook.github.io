ba88fb1ba7201e79776d7b474a6867837e21de79
Thomas Crook <stu.thomas.crook@ihsnola.org>
2021-11-22 20:18:56 +0000

    circularity2

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -49,12 +49,12 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-            physikz.updatePosition([0]);
-            physikz.updatePosition([1]);
-            physikz.updatePosition([2]);
-            physikz.updatePosition([3]);
-            physikz.updatePosition([4]);
-
+            physikz.updatePosition(circles[0]);
+            physikz.updatePosition(circles[1]);
+            physikz.updatePosition(circles[2]);
+            physikz.updatePosition(circles[3]);
+            physikz.updatePosition(circles[4]);
+        
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
            
 
